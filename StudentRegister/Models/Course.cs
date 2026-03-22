e044709f62077cd7dc4bd6b2ffd049a796b62fab
Harish-18-git <harishsarabi18@gmail.com>
2026-03-22 14:24:04 +0530

    First Commit

---
+++ StudentRegister/Models/Course.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentRegister.Models
+{
+    public class Course
+    {
+        [Key]
+        public int Id { get; set; }
+        public string name { get; set; }
+
+        public List<Student> Students { get; set; }
+        
+    }
+}
---
+++ StudentRegister/Models/StudentRegisterDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StudentRegister.Models
+{
+    public class StudentRegisterDbContext:DbContext
+    {
+        public StudentRegisterDbContext(DbContextOptions<StudentRegisterDbContext> options) : base(options)
+        {
+        }   
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Topic> Topics { get; set; }
+
+       
+    }
+}
