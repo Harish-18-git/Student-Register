1b7a25c26c26facf5048a61d20f16a1b464908dd
Harish-18-git <harishsarabi18@gmail.com>
2026-03-26 22:10:42 +0530

    first commit student register project

---
+++ Services/ICourseService.cs
@@ -0,0 +1,10 @@
+﻿using StudentRegister.Models;
+
+namespace StudentRegister.NewFolder
+{
+    public interface ICourseService
+    {
+        List<Course> GetAllCourses();
+        Course GetCourseByFName(string name);
+    }
+}
---
+++ Services/IStudentService.cs
@@ -0,0 +1,13 @@
+﻿using StudentRegister.Models;
+
+namespace StudentRegister.NewFolder
+{
+    public interface IStudentService
+    {
+        void AddStudent(Student student);
+        void UpdateStudent(Student student);
+        void DeleteStudent(int id);
+        Student GetStudentById(int id);
+        List<Student> GetAllStudent();
+    }
+}
